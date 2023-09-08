ef6c0dc720923ac3289e8ca805722b78b1fa09a5
hungtq1016 <hungtq1016@gmail.com>
2023-09-08 21:01:06 +0700

    up

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -9,18 +9,9 @@ import axios from 'axios';
 import { ref } from 'vue';
 import { v4 as uuid } from 'uuid';
 const data = ref()
-const brands =  ref()
-const models =  ref()
+
 const test = async () => {
-    await axios.get('http://localhost:8000/api/brand').then(res=>{
-        brands.value = res.data.data
-        
-    }
-    ).catch(err=>console.log(err)
-    )
-
-
-
+ 
     await axios.get('https://m-car.mioto.vn/homepage/sd').then(res=>{
         data.value = res.data.data.dealArounds
     }
@@ -28,17 +19,25 @@ const test = async () => {
     )
     console.log(data.value);
     
-    const imgs:any[] =[]
-    data.value.map((item:any)=>{
+    data.value.map(async (item:any)=>{
+        let imgs:any[] =[]
         item.photos.forEach((element:any) => {
-            let imgid = uuid()
-            imgs.push({
-                id:imgid,
-                src:element.fullUrl
-            })
+            imgs.push(element.fullUrl)
         });
+        let payload = {
+            car_name:item.name,
+            seats:item.seat,
+            electric : item.deliveryEnable,
+            gear: item.optionsTransmission,
+            images : imgs,
+        }
+        await axios.post('http://localhost:8000/api/car',payload).then(res=>{
+            console.log(res);
+            
+    }
+    ).catch(err=>console.log(err)
+    )
     })
-    console.log(brands.value);
     
 }
 test()
